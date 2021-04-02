5b566c4aa6791c0c5ae845e423160a8df800fd1e
Borkowski, Mark Daniel <71045065+ToxiicMarker69@users.noreply.github.com>
2021-04-01 23:11:54 -0400

    Part 5 Fog Effect

---
+++ UnderWaterWorldShaders/Assets/Scripts/FogEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class FogEffect : MonoBehaviour
+{
+    public Material _mat;
+    public Color _fogColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mat.SetColor("_FogColor", _fogColor);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, _mat);
+    }
+}
--- UnderWaterWorldShaders/Assets/Scripts/UnderwaterEffect.cs
+++ UnderWaterWorldShaders/Assets/Scripts/UnderwaterEffect.cs
@@ -6,6 +6,15 @@ using UnityEngine;
 public class UnderwaterEffect : MonoBehaviour
 {
     public Material _mat;
+
+    [Range(0.001f, 0.1f)]
+    public float _pixelOffset;
+    [Range(0.1f, 20f)]
+    public float _noiseScale;
+    [Range(0.1f, 20f)]
+    public float _noiseFrequency;
+    [Range(0.1f, 30f)]
+    public float _noiseSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +24,11 @@ public class UnderwaterEffect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _mat.SetFloat("_NoiseFrequency", _noiseFrequency);
+        _mat.SetFloat("_NoiseSpeed", _noiseSpeed);
+        _mat.SetFloat("_NoiseScale", _noiseScale);
+        _mat.SetFloat("_PixelOffset", _pixelOffset);
+
     }
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
