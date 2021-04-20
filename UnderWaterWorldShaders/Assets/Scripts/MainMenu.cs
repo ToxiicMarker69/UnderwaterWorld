e8b514855ed9bfbddfc5710e979dde878a7b8ea0
Borkowski, Mark Daniel <71045065+ToxiicMarker69@users.noreply.github.com>
2021-04-19 23:14:04 -0400

    MainMenu Added

---
+++ UnderWaterWorldShaders/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame ()
+   {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+
+   public void QuitGame ()
+   {
+       Debug.Log("QUIT"); //just a notification
+       Application.Quit(); //won't work in unity editor
+   }
+}
